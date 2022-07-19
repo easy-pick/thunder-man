1703883e7f27ac64e829681fda60e17f59a1ee38
Kim Sang Hyun <tkdgus403@naver.com>
2022-07-19 12:59:10 +0900

    #1 프로젝트 최소 커밋
    https://start.spring.io/ 프로젝트 생성후 최초 커밋.
    #1 closed

---
+++ src/main/java/com/easypick/thunderMan/ThunderManApplication.java
@@ -0,0 +1,13 @@
+package com.easypick.thunderMan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThunderManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThunderManApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easypick/thunderMan/ThunderManApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easypick.thunderMan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThunderManApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
