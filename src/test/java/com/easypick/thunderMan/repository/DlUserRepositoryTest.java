a57c421d6a97c91c4bed4b82ef99959a37c011eb
SangHyun Kim <tkdgus403@naver.com>
2022-07-29 10:40:57 +0900

    #12 클론코딩 Close
    이후부터도 클론코딩이지만 하나의 이슈로는 너무 방대해서 분할예정.
    git merge하면서 누락된 파일 다시 커밋.

--- src/test/java/com/easypick/thunderMan/repository/DlUserRepositoryTest.java
+++ src/test/java/com/easypick/thunderMan/repository/DlUserRepositoryTest.java
@@ -3,6 +3,7 @@ package com.easypick.thunderMan.repository;
 
 import com.easypick.thunderMan.config.JpaConfig;
 import com.easypick.thunderMan.domain.DlUser;
+import com.easypick.thunderMan.dto.SignupDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,6 +39,37 @@ public class DlUserRepositoryTest {
 
     }
 
+    @DisplayName("User 회원가입 Test")
+    @Test
+    void givenSignUpUserInfo_whenSaving_thenReturnsUserCreate() {
 
+
+        // Given
+        String userId = "testUser1234";
+        String password = "testUser1234";
+        String email = "test@test.com";
+        String phone = "01012345678";
+        String nickName = "테스트유저";
+
+        SignupDto signupDto = new SignupDto();
+
+        signupDto.setUserId(userId);
+        signupDto.setPassword(password);
+        signupDto.setEmail(email);
+        signupDto.setPhone(phone);
+        signupDto.setNickname(nickName);
+
+        DlUser signUpUser = DlUser.of(signupDto);
+
+        // When
+
+        dlUserRepository.save(signUpUser);
+        DlUser user = dlUserRepository.findDlUserByUserId(userId);
+
+        // Then
+        System.out.println("user :::::::::: " + user.toString());
+        assertThat(user).isEqualTo(signUpUser);
+
+    }
 
 }
