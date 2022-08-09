9e99a29bb646e81ef9e6d1006a5fcbe17067b88c
SangHyun Kim <tkdgus403@naver.com>
2022-08-09 15:19:16 +0900

    #14 -Food관련 작업진행
    웹에서 해당음식 누를경우 팝업창 출력되면서 호출하는 api에 맞게 클래스 및 메소드 추가

--- src/main/java/com/easypick/thunderMan/repository/FoodRepository.java
+++ src/main/java/com/easypick/thunderMan/repository/FoodRepository.java
@@ -2,18 +2,23 @@ package com.easypick.thunderMan.repository;
 
 import com.easypick.thunderMan.domain.DlFood;
 import com.easypick.thunderMan.domain.QDlFood;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.querydsl.binding.QuerydslBindings;
+
+import java.util.List;
 
 public interface FoodRepository extends
         JpaRepository<DlFood, Long>
         , QuerydslPredicateExecutor<DlFood>
         , QuerydslBinderCustomizer<QDlFood> {
 
-    Slice<DlFood> findByDlStoreId(Long DlStoreId);
+    List<DlFood> findByDlStoreId(Long DlStoreId);
+//    List<DlFoodWithFoodOptionsDto>
+
+
+
 
 
 
--- src/main/java/com/easypick/thunderMan/service/FoodServiceImpl.java
+++ src/main/java/com/easypick/thunderMan/service/FoodServiceImpl.java
@@ -1,11 +1,43 @@
 package com.easypick.thunderMan.service;
 
+import com.easypick.thunderMan.dto.DlFoodDto;
+import com.easypick.thunderMan.dto.DlFoodOptionDto;
 import com.easypick.thunderMan.dto.DlFoodWithFoodOptionsDto;
+import com.easypick.thunderMan.repository.FoodRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+
+@Slf4j
+@Service
 public class FoodServiceImpl implements FoodService {
 
+    @Autowired
+    FoodRepository foodRepository;
+    @Transactional(readOnly = true)
     public DlFoodWithFoodOptionsDto storeFoodList(Long storeId){
 
+        log.info("검색값 : " + foodRepository.findByDlStoreId(storeId));
+//        List<DlFoodWithFoodOptionsDto> foodWithFoodOptionsDtos = foodRepository.findByDlStoreId(storeId).stream().toList();
+//        log.info("결과값 DlFoodWithFoodOptionsDto : " + foodRepository.findByDlStoreId(storeId).map(DlFoodWithFoodOptionsDto::from));
         return null;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<DlFoodOptionDto> getFoodOptions(Long foodId) {
+
+        List<DlFoodOptionDto> dlFoodOptionDtoList;
+
+        DlFoodDto dlFood = foodRepository.findById(foodId).map(DlFoodDto::from)
+                .orElseThrow(() -> new EntityNotFoundException("해당 foodId가 없습니다. foodId = " + foodId));
+        dlFoodOptionDtoList = dlFood.dlFoodOption().stream().toList();
+        log.info("foodId 의 OptionList : " + dlFoodOptionDtoList );
+
+        return dlFoodOptionDtoList;
     }
 }
