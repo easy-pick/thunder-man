7e5aa6596415afbe1bd759b4902416879bbd9cb4
SangHyun Kim <tkdgus403@naver.com>
2022-08-09 15:20:37 +0900

    #14 - 상점 디테일 관련 Back-End 로직수정
    서비스로직 및 return을 위한 responseDto 수정.

--- src/main/java/com/easypick/thunderMan/service/StoreServiceImpl.java
+++ src/main/java/com/easypick/thunderMan/service/StoreServiceImpl.java
@@ -7,9 +7,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -22,10 +25,11 @@ public class StoreServiceImpl implements StoreService{
     FoodRepository foodRepository;
 
     @Override
+    @Transactional(readOnly = true)
     public Slice<DlStoreDto> storeList(long category, long address) {
 
         Slice<DlStoreDto> dlStores = storeRepository.findByCategory(category).map(DlStoreDto::from);
-        log.debug("검색된 List<DlStoreDto> 의 값 :: {}" + dlStores);
+        log.debug("검색된 List<DlStoreDto> 의 값 :: {}" + dlStores.toString());
 
         return dlStores;
     }
@@ -42,13 +46,19 @@ public class StoreServiceImpl implements StoreService{
     }
 
     @Override
+    @Transactional(readOnly = true)
     public DlStoreWithFoodsDto storeDetail(long storeId) {
 
         DlStoreDto storeDto = storeRepository.findById(storeId).map(DlStoreDto::from)
                 .orElseThrow(() -> new EntityNotFoundException("잘못된 storeId 입니다 - " + storeId));
 
-        Slice<DlFoodWithFoodOptionsDto> foodWithFoodOptionsDtos = foodRepository.findByDlStoreId(storeId).map(DlFoodWithFoodOptionsDto::from);
+        log.info("storeDto : " + storeDto);
+        log.info("검색값 : " + foodRepository.findByDlStoreId(storeId));
 
-        return DlStoreWithFoodsDto.of(storeDto, foodWithFoodOptionsDtos);
+        DlStoreWithFoodsDto dlStoreWithFoodsDto = DlStoreWithFoodsDto.of(storeDto, foodRepository.findByDlStoreId(storeId).stream()
+                .map(DlFoodWithFoodOptionsDto::from)
+                .collect(Collectors.toCollection(LinkedHashSet::new)));
+
+        return dlStoreWithFoodsDto;
     }
 }
